448cecb1219d558e06a0ed3a68276080f8328343
WalhaManel <manel.walha@esen.tn>
2023-11-27 01:46:26 +0100

    MVC

--- src/main/java/tn/esprit/firstproject/Service/FoyerService.java
+++ src/main/java/tn/esprit/firstproject/Service/FoyerService.java
@@ -1,4 +1,32 @@
 package tn.esprit.firstproject.Service;
 
+import tn.esprit.firstproject.Entity.Foyer;
+
+import java.util.List;
+
 public class FoyerService implements IFoyerService{
+    @Override
+    public List<Foyer> retrieveAllFoyers() {
+        return null;
+    }
+
+    @Override
+    public Foyer addFoyer(Foyer f) {
+        return null;
+    }
+
+    @Override
+    public Foyer updateFoyer(Foyer f) {
+        return null;
+    }
+
+    @Override
+    public Foyer retrieveFoyer(long idFoyer) {
+        return null;
+    }
+
+    @Override
+    public void archiverFoyer(long idFoyer) {
+
+    }
 }
--- src/main/java/tn/esprit/firstproject/Service/UniversiteService.java
+++ src/main/java/tn/esprit/firstproject/Service/UniversiteService.java
@@ -1,4 +1,32 @@
 package tn.esprit.firstproject.Service;
 
+import tn.esprit.firstproject.Entity.Universite;
+
+import java.util.List;
+
 public class UniversiteService implements IUniversiteService{
+    @Override
+    public List<Universite> retrieveAllUniversities() {
+        return null;
+    }
+
+    @Override
+    public Universite addUniversity(Universite u) {
+        return null;
+    }
+
+    @Override
+    public Universite updateUniversity(Universite u) {
+        return null;
+    }
+
+    @Override
+    public Universite retrieveUniversity(long idUniversity) {
+        return null;
+    }
+
+    @Override
+    public void removeUniversity(long idUniversity) {
+
+    }
 }
