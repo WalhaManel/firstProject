48bfcf66ff6101b06b920cad693d9b39d18bab2a
WalhaManel <manel.walha@esen.tn>
2023-11-27 01:43:37 +0100

    01

--- src/main/java/tn/esprit/firstproject/Service/IFoyerService.java
+++ src/main/java/tn/esprit/firstproject/Service/IFoyerService.java
@@ -1,4 +1,13 @@
 package tn.esprit.firstproject.Service;
 
+import tn.esprit.firstproject.Entity.Foyer;
+
+import java.util.List;
+
 public interface IFoyerService {
+    List<Foyer> retrieveAllFoyers();
+    Foyer addFoyer (Foyer f);
+    Foyer updateFoyer (Foyer f);
+    Foyer retrieveFoyer (long idFoyer);
+    void archiverFoyer (long idFoyer);
 }
--- src/main/java/tn/esprit/firstproject/Service/IUniversiteService.java
+++ src/main/java/tn/esprit/firstproject/Service/IUniversiteService.java
@@ -1,4 +1,13 @@
 package tn.esprit.firstproject.Service;
 
+import tn.esprit.firstproject.Entity.Universite;
+
+import java.util.List;
+
 public interface IUniversiteService {
+    List<Universite> retrieveAllUniversities();
+    Universite addUniversity (Universite u);
+    Universite updateUniversity (Universite u);
+    Universite retrieveUniversity (long idUniversity);
+    void removeUniversity  (long idUniversity );
 }
